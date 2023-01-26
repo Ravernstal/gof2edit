181c34bb1efb9e414e91fdeaff6ffc627a6a7f98
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2023-01-26 17:24:39 +0000

    Add patching functionality

--- src/systems.rs
+++ src/systems.rs
@@ -1,10 +1,15 @@
-use crate::{Faction, SecurityLevel, System};
+use crate::data::faction::Faction;
+use crate::data::security_level::SecurityLevel;
+use crate::data::system::System;
+use crate::patch;
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::ErrorKind;
 use std::ops::Not;
 use std::path::Path;
 use std::{fs, io};
+
+const SYSTEM_COUNT_ADDRESSES: &[u64] = &[0x000C69A8, 0x000C69BA, 0x00135C66, 0x00135E80];
 
 pub fn unpack(
     input_filepath: impl AsRef<Path>,
@@ -27,7 +32,7 @@ pub fn unpack(
     serde_json::to_string_pretty(&systems).map(|string| fs::write(output_filepath, string))??;
 
     println!(
-        "Unpacked {} systems to {}",
+        "Unpacked {} systems into {}",
         count,
         output_filepath.display()
     );
@@ -56,9 +61,33 @@ pub fn repack(
         .try_for_each(|system| write_one(&mut file, system))?;
 
     println!(
-        "Repacked {} systems to {}",
+        "Repacked {} systems into {}",
         systems.len(),
         output_filepath.display()
+    );
+
+    Ok(())
+}
+
+pub fn patch(json_filepath: impl AsRef<Path>, so_filepath: impl AsRef<Path>) -> io::Result<()> {
+    let json_filepath = json_filepath.as_ref();
+    let so_filepath = so_filepath.as_ref();
+
+    println!("Reading systems from {} ...", json_filepath.display());
+
+    let json_string = fs::read_to_string(json_filepath)?;
+    let system_count = serde_json::from_str::<Vec<System>>(&json_string)?.len() as u8;
+
+    let mut file = OpenOptions::new().write(true).open(so_filepath)?;
+
+    SYSTEM_COUNT_ADDRESSES
+        .iter()
+        .try_for_each(|address| patch::set_byte(&mut file, *address, system_count))?;
+
+    println!(
+        "Patched {} systems into {}",
+        system_count,
+        so_filepath.display()
     );
 
     Ok(())
