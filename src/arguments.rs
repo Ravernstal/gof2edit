d657128b240c2448f5434b842f9afb449da1868d
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2023-01-27 00:06:47 +0000

    Add lang string functionality

--- src/arguments.rs
+++ src/arguments.rs
@@ -3,26 +3,28 @@ use std::path::PathBuf;
 
 #[derive(Debug, Subcommand)]
 pub enum Action {
-    UnpackSystems {
-        input_filepath: PathBuf,
-    },
-    RepackSystems {
-        input_filepath: PathBuf,
-    },
+    /// Unpack systems.bin into JSON format
+    UnpackSystems { input_filepath: PathBuf },
+    /// Repack systems.json into BIN format
+    RepackSystems { input_filepath: PathBuf },
+    /// Patch binary with systems.json
     PatchSystems {
         json_filepath: PathBuf,
         so_filepath: PathBuf,
     },
-    UnpackStations {
-        input_filepath: PathBuf,
-    },
-    RepackStations {
-        input_filepath: PathBuf,
-    },
+    /// Unpack stations.bin into JSON format
+    UnpackStations { input_filepath: PathBuf },
+    /// Repack stations.json into BIN format
+    RepackStations { input_filepath: PathBuf },
+    /// Patch binary with stations.json
     PatchStations {
         json_filepath: PathBuf,
         so_filepath: PathBuf,
     },
+    /// Unpack LANG file into JSON format
+    UnpackLang { input_filepath: PathBuf },
+    /// Repack JSON file into LANG format
+    RepackLang { input_filepath: PathBuf },
 }
 
 #[derive(Debug, Parser)]
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,4 +1,5 @@
 pub mod faction;
+pub mod lang_string;
 pub mod security_level;
 pub mod station;
 pub mod system;
