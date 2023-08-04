bf0079fca8e0cdc260f041d03dd620dd2cb9a131
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2023-08-05 00:24:21 +0100

    Move ship serialisation code into ship module

--- src/unpackers/ship.rs
+++ src/unpackers/ship.rs
@@ -1,5 +1,6 @@
+use crate::bin_io::read::BinReader;
 use crate::data::ship::Ship;
-use byteorder::{BigEndian, ReadBytesExt};
+use byteorder::BigEndian;
 use std::fs::File;
 use std::path::Path;
 use std::{fs, io};
@@ -14,10 +15,10 @@ pub fn unpack(
     println!("Unpacking ships from {} ...", input_filepath.display());
 
     let mut file = File::open(input_filepath)?;
-    let mut ships = vec![];
+    let mut ships: Vec<Ship> = vec![];
     let mut count = 0;
 
-    while let Ok(ship) = read_one(&mut file) {
+    while let Ok(ship) = file.read_bin::<BigEndian>() {
         ships.push(ship);
         count += 1
     }
@@ -31,28 +32,4 @@ pub fn unpack(
     );
 
     Ok(())
-}
-
-fn read_one(source: &mut impl ReadBytesExt) -> io::Result<Ship> {
-    let index = source.read_u32::<BigEndian>()?;
-    let armour = source.read_u32::<BigEndian>()?;
-    let cargo_capacity = source.read_u32::<BigEndian>()?;
-    let price = source.read_u32::<BigEndian>()?;
-    let primary_weapon_count = source.read_u32::<BigEndian>()?;
-    let secondary_weapon_count = source.read_u32::<BigEndian>()?;
-    let turret_count = source.read_u32::<BigEndian>()?;
-    let equipment_slot_count = source.read_u32::<BigEndian>()?;
-    let handling = source.read_u32::<BigEndian>()?;
-
-    Ok(Ship {
-        index,
-        armour,
-        cargo_capacity,
-        price,
-        primary_weapon_count,
-        secondary_weapon_count,
-        turret_count,
-        equipment_slot_count,
-        handling,
-    })
 }
