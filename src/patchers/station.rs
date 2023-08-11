a93cd1bd69c568ae16ce66f55d1673b07bdc1a39
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2023-08-11 14:27:28 +0100

    Refactor patching code

--- src/patchers/station.rs
+++ src/patchers/station.rs
@@ -2,41 +2,32 @@ use crate::data::station::Station;
 use crate::patch;
 use crate::patch_addresses::binary_version::BinaryVersion;
 use crate::patch_addresses::station;
-use std::fs::OpenOptions;
-use std::io::ErrorKind;
+use std::io;
 use std::ops::Not;
 use std::path::Path;
-use std::{fs, io};
 
 pub fn patch(
     json_filepath: impl AsRef<Path>,
-    so_filepath: impl AsRef<Path>,
+    binary_filepath: impl AsRef<Path>,
     binary: BinaryVersion,
     silent: bool,
 ) -> io::Result<()> {
     let json_filepath = json_filepath.as_ref();
-    let so_filepath = so_filepath.as_ref();
+    let binary_filepath = binary_filepath.as_ref();
 
     if silent.not() {
         println!("Reading stations from {} ...", json_filepath.display());
     }
 
-    let json_string = fs::read_to_string(json_filepath)?;
-    let station_count = serde_json::from_str::<Vec<Station>>(&json_string)?.len();
-    let station_count = station_count
-        .try_into()
-        .map_err(|_| ErrorKind::InvalidData)?;
+    let address_modifiers = station::addresses(binary);
 
-    let mut file = OpenOptions::new().write(true).open(so_filepath)?;
-    let addresses = station::addresses(binary);
-
-    patch::address_list_modifiers(&mut file, addresses, station_count)?;
+    let count =
+        patch::file_with_counts::<Station>(json_filepath, binary_filepath, address_modifiers)?;
 
     if silent.not() {
         println!(
-            "Patched {} stations into {}",
-            station_count,
-            so_filepath.display()
+            "Patched {count} stations into {}",
+            binary_filepath.display()
         );
     }
 
--- src/patchers/system.rs
+++ src/patchers/system.rs
@@ -2,42 +2,30 @@ use crate::data::system::System;
 use crate::patch;
 use crate::patch_addresses::binary_version::BinaryVersion;
 use crate::patch_addresses::system;
-use std::fs::OpenOptions;
-use std::io::ErrorKind;
+use std::io;
 use std::ops::Not;
 use std::path::Path;
-use std::{fs, io};
 
 pub fn patch(
     json_filepath: impl AsRef<Path>,
-    so_filepath: impl AsRef<Path>,
+    binary_filepath: impl AsRef<Path>,
     binary: BinaryVersion,
     silent: bool,
 ) -> io::Result<()> {
     let json_filepath = json_filepath.as_ref();
-    let so_filepath = so_filepath.as_ref();
+    let binary_filepath = binary_filepath.as_ref();
 
     if silent.not() {
         println!("Reading systems from {} ...", json_filepath.display());
     }
 
-    let json_string = fs::read_to_string(json_filepath)?;
-    let system_count = serde_json::from_str::<Vec<System>>(&json_string)?.len();
-    let system_count = system_count
-        .try_into()
-        .map_err(|_| ErrorKind::InvalidData)?;
+    let address_modifiers = system::addresses(binary);
 
-    let mut file = OpenOptions::new().write(true).open(so_filepath)?;
-    let addresses = system::addresses(binary);
-
-    patch::address_list_modifiers(&mut file, addresses, system_count)?;
+    let count =
+        patch::file_with_counts::<System>(json_filepath, binary_filepath, address_modifiers)?;
 
     if silent.not() {
-        println!(
-            "Patched {} systems into {}",
-            system_count,
-            so_filepath.display()
-        );
+        println!("Patched {count} systems into {}", binary_filepath.display());
     }
 
     Ok(())
