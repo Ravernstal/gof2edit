08e21c9833ffc0401a478c23bcc1f17b3dc83a59
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2023-08-28 00:30:56 +0100

    Add BIN-to-JSON tests

---
+++ tests/unpack.rs
@@ -0,0 +1,90 @@
+use gof2edit::data::{Item, LangString, Ship, Station, System};
+use std::io::Cursor;
+use std::ops::Not;
+
+const ITEMS_JSON: &str = include_str!("data/items.json");
+const LANG_JSON: &str = include_str!("data/lang.json");
+const SHIPS_JSON: &str = include_str!("data/ships.json");
+const STATIONS_JSON: &str = include_str!("data/stations.json");
+const SYSTEMS_JSON: &str = include_str!("data/systems.json");
+
+#[test]
+fn convert_items_bin_to_json() {
+    let items =
+        serde_json::from_str::<Vec<Item>>(ITEMS_JSON).expect("failed to parse item test JSON");
+
+    let mut buffer = Cursor::new(vec![0u8]);
+
+    gof2edit::write_object_list(&mut buffer, items).expect("failed to repack items to BIN");
+    buffer.set_position(0);
+
+    let items =
+        gof2edit::read_object_list::<Item>(&mut buffer).expect("failed to unpack items from BIN");
+
+    assert!(items.is_empty().not())
+}
+
+#[test]
+fn convert_lang_bin_to_json() {
+    let lang_strings = serde_json::from_str::<Vec<LangString>>(LANG_JSON)
+        .expect("failed to parse lang string test JSON");
+
+    let mut buffer = Cursor::new(vec![0u8]);
+
+    gof2edit::write_object_list(&mut buffer, lang_strings)
+        .expect("failed to repack lang strings to BIN");
+    buffer.set_position(0);
+
+    let lang_strings = gof2edit::read_object_list::<LangString>(&mut buffer)
+        .expect("failed to unpack lang strings from BIN");
+
+    assert!(lang_strings.is_empty().not())
+}
+
+#[test]
+fn convert_ships_bin_to_json() {
+    let ships =
+        serde_json::from_str::<Vec<Ship>>(SHIPS_JSON).expect("failed to parse ship test JSON");
+
+    let mut buffer = Cursor::new(vec![0u8]);
+
+    gof2edit::write_object_list(&mut buffer, ships).expect("failed to repack ships to BIN");
+    buffer.set_position(0);
+
+    let ships =
+        gof2edit::read_object_list::<Ship>(&mut buffer).expect("failed to unpack ships from BIN");
+
+    assert!(ships.is_empty().not())
+}
+
+#[test]
+fn convert_stations_bin_to_json() {
+    let stations = serde_json::from_str::<Vec<Station>>(STATIONS_JSON)
+        .expect("failed to parse station test JSON");
+
+    let mut buffer = Cursor::new(vec![0u8]);
+
+    gof2edit::write_object_list(&mut buffer, stations).expect("failed to repack stations to BIN");
+    buffer.set_position(0);
+
+    let stations = gof2edit::read_object_list::<Station>(&mut buffer)
+        .expect("failed to unpack stations from BIN");
+
+    assert!(stations.is_empty().not())
+}
+
+#[test]
+fn convert_systems_bin_to_json() {
+    let systems = serde_json::from_str::<Vec<System>>(SYSTEMS_JSON)
+        .expect("failed to parse system test JSON");
+
+    let mut buffer = Cursor::new(vec![0u8]);
+
+    gof2edit::write_object_list(&mut buffer, systems).expect("failed to repack systems to BIN");
+    buffer.set_position(0);
+
+    let systems = gof2edit::read_object_list::<System>(&mut buffer)
+        .expect("failed to unpack systems from BIN");
+
+    assert!(systems.is_empty().not())
+}
