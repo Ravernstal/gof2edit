ece0e87323b6b0e1b471722985a42fdc8338ad0b
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2024-04-24 23:53:36 +0100

    Add author field to binary patch

--- src/binary_patch.rs
+++ src/binary_patch.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 #[derive(Deserialize, Serialize)]
 pub struct BinaryPatch {
     pub name: String,
+    pub author: String,
     pub description: String,
     pub addresses: HashMap<BinaryVersion, HashMap<u64, u8>>,
 }
