1c36632422583335e47db0c334418a7d10031a8f
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2024-04-25 00:01:45 +0100

    Add bloom patch

--- src/binary_patch.rs
+++ src/binary_patch.rs
@@ -1,11 +1,11 @@
 use crate::binary_version::BinaryVersion;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 #[derive(Deserialize, Serialize)]
 pub struct BinaryPatch {
     pub name: String,
     pub author: String,
     pub description: String,
-    pub addresses: HashMap<BinaryVersion, HashMap<u64, u8>>,
+    pub addresses: HashMap<BinaryVersion, BTreeMap<u64, u8>>,
 }
