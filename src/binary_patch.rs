a5569a746e648736e4aadea60e8b6a1504e59726
Ravernstal <110842049+Ravernstal@users.noreply.github.com>
2024-04-25 23:42:15 +0100

    Add default binary patch values

--- src/binary_patch.rs
+++ src/binary_patch.rs
@@ -2,10 +2,25 @@ use crate::binary_version::BinaryVersion;
 use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, HashMap};
 
+const DEFAULT_NAME: &str = "<name>";
+const DEFAULT_AUTHOR: &str = "<author>";
+const DEFAULT_DESCRIPTION: &str = "<description>";
+
 #[derive(Deserialize, Serialize)]
 pub struct BinaryPatch {
     pub name: String,
     pub author: String,
     pub description: String,
     pub addresses: HashMap<BinaryVersion, BTreeMap<u64, u8>>,
+}
+
+impl Default for BinaryPatch {
+    fn default() -> Self {
+        Self {
+            name: DEFAULT_NAME.to_string(),
+            author: DEFAULT_AUTHOR.to_string(),
+            description: DEFAULT_DESCRIPTION.to_string(),
+            addresses: Default::default(),
+        }
+    }
 }
